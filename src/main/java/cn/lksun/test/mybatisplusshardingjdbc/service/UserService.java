074fd033e2f1fcf760832fbe840f2085ceb969ba
Canon <717437709@qq.com>
2020-12-02 10:46:50 +0800

    条件构造器查询

--- src/main/java/cn/lksun/test/mybatisplusshardingjdbc/service/UserService.java
+++ src/main/java/cn/lksun/test/mybatisplusshardingjdbc/service/UserService.java
@@ -2,7 +2,6 @@ package cn.lksun.test.mybatisplusshardingjdbc.service;
 
 import cn.lksun.test.mybatisplusshardingjdbc.entity.User;
 import com.baomidou.mybatisplus.extension.service.IService;
-import org.springframework.stereotype.Service;
 
 
 public interface UserService extends IService<User> {
--- src/test/java/cn/lksun/test/mybatisplusshardingjdbc/MybatisPlusShardingjdbcApplicationTests.java
+++ src/test/java/cn/lksun/test/mybatisplusshardingjdbc/MybatisPlusShardingjdbcApplicationTests.java
@@ -1,13 +1,11 @@
 package cn.lksun.test.mybatisplusshardingjdbc;
 
-import cn.lksun.test.mybatisplusshardingjdbc.entity.User;
 import cn.lksun.test.mybatisplusshardingjdbc.mapper.UserMapper;
 import cn.lksun.test.mybatisplusshardingjdbc.service.UserService;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import java.util.ArrayList;
 
 @SpringBootTest
 class MybatisPlusShardingjdbcApplicationTests {
@@ -20,6 +18,7 @@ class MybatisPlusShardingjdbcApplicationTests {
 
     @Test
     void contextLoads() {
+        /*
         // 插入100条数据,状态为1-4随机取
         int [] arr = {1,2,3,4};
         ArrayList<User> userList = new ArrayList<User>();
@@ -28,22 +27,29 @@ class MybatisPlusShardingjdbcApplicationTests {
             user.setName("游客"+i);
             user.setStatus(arr[(int)(Math.random()*arr.length)]);
             userList.add(user);
+            // (1) Mapper CURD 每次添加一条数据
+            // userMapper.insert(user);
         }
+        */
 
-        System.out.println(userMapper.getNameById(1333956965229178881L));
-        //userMapper.insert(user);
-//        userService.saveBatch(userList);
-        // 获取所有数据
-        //System.out.println(userMapper.selectList(null));
+        // (2) Mapper CURD 批量添加
+        // userService.saveBatch(userList);
 
-//        System.out.println(userMapper.selectById(1333698701316882434L));
-//        System.out.println(userMapper.selectById(1333698701459488770L));
-//        System.out.println(userMapper.selectById(1333698701648232449L));
-//        System.out.println(userMapper.selectById(1333698702273183747L));
+        // (3) Mapper CURD 获取所有数据
+        // System.out.println(userMapper.selectList(null));
 
-//        HashMap<String, Object> map = new HashMap<String, Object>();
-//        map.put("name","游客13");
-//        System.out.println(userMapper.selectByMap(map));
+
+        // (4) Mapper CURD 自定义Mapper
+        // System.out.println(userMapper.getNameById(1333956965229178881L));
+
+        // (5) Mapper CURD 通过Map条件查询
+        // HashMap<String, Object> map = new HashMap<String, Object>();
+        // map.put("name","游客13");
+        // System.out.println(userMapper.selectByMap(map));
+
+        // (6) Service CURD 通过条件构造器条件查询
+        // QueryWrapper<User> userQueryWrapper = new QueryWrapper<>();
+        // System.out.println(userService.getOne(userQueryWrapper.eq("name", "游客25")));
 
     }
 
